5cbd4be054f667fe48847ac388e52b2559032db5
Andras Szell <andrew@Andrews-Mac-Pro.local>
2014-03-16 19:04:05 +0100

    Routinok hozzaadva

--- led.asm
+++ led.asm
@@ -1,3 +1,55 @@
-;*************************
+;*********** AUTOS LED SUBROUTINOK **************
+
 SET_AUTOS_ZOLD:
-	
+	in	temp, PORTC 		; Port betoltese
+	ori	temp, 0b0000100		; Led bekapcsolasa
+	andi temp, 0b1111100	;
+	out PORTC, temp			; Portra iras
+ret	
+;*************************
+
+SET_AUTOS_PIROS:
+; TODO
+ret
+;*************************
+
+SET_AUTOS_PIROS_SARGA:
+; TODO
+ret
+;*************************
+
+SET_AUTOS_SARGA:
+; TODO
+ret
+;*************************
+
+SET_AUTOS_SARGA_V:
+; TODO
+ret
+
+;*********** GYALOGOS LED SUBROUTINOK **************
+
+SET_GYALOGOS_ZOLD:
+; TODO
+ret	
+;*************************
+
+SET_GYALOGOS_PIROS:
+; TODO
+ret
+;*************************
+
+SET_GYALOGOS_PIROS_SARGA:
+; TODO
+ret
+;*************************
+
+SET_GYALOGOS_SARGA:
+; TODO
+ret
+;*************************
+
+SET_GYALOGOS_SARGA_V:
+; TODO
+ret
+
